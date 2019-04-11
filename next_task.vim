03861ed490b3ab9ea654960ba55309f8330001a1
Dathan Bennett <dathan@gmail.com>
2019-04-10 20:44:37 -0700

    Refactor and add previous task support

--- next_task.vim
+++ next_task.vim
@@ -11,10 +11,23 @@ function GoToNextTask()
   " TODO: this is hacky and may work incorrectly with other line endings. If we notice issues, pick another approach
   let buf=join(getline(1, '$'), "\n")
   " Find the location of the next task
-  let new_offset=system("python ~/dev/task-search/task_search.py " . byte_offset, buf)
+  let new_offset=system("python ~/dev/task-search/task_search.py next " . byte_offset, buf)
   " Position the cursor at the next task
-  exec "goto " . (new_offset + 1)
+  execute "normal! :goto " . (new_offset + 1) . "\<cr>"
+endfunction
+
+function GoToPreviousTask()
+  " Get the current byte offset
+  let byte_offset=line2byte(line('.'))+col('.')-2
+  " Get the contents of the current buffer
+  " TODO: this is hacky and may work incorrectly with other line endings. If we notice issues, pick another approach
+  let buf=join(getline(1, '$'), "\n")
+  " Find the location of the next task
+  let new_offset=system("python ~/dev/task-search/task_search.py previous " . byte_offset, buf)
+  " Position the cursor at the next task
+  execute "normal! :goto " . (new_offset + 1) . "\<cr>"
 endfunction
 
 nmap <leader>n :call GoToNextTask()<CR>
+nmap <leader>N :call GoToPreviousTask()<CR>
 
--- tests.py
+++ tests.py
@@ -2,20 +2,24 @@ import unittest
 import task_search
 
 
-class FindTaskTest(unittest.TestCase):
+class FindNextTaskTest(unittest.TestCase):
     def test_ignores_text_before_starting_offset(self):
-        result = task_search.find_task(2, "- [ ] first\n- [ ] second")
+        result = task_search.find_next_task(2, "- [ ] first\n- [ ] second")
         self.assertEqual(15, result, "Should ignore the leading line; expected 2, got {}".format(result))
 
     def test_when_no_next_task_returns_original_offset(self):
-        result = task_search.find_task(2, "foo")
+        result = task_search.find_next_task(2, "foo")
         self.assertEqual(2, result)
 
 
-class FindTaskHelperTest(unittest.TestCase):
-    def test_when_searching_from_the_start_of_a_task_it_skips_it(self):
-        result = task_search.find_task_helper(0, 0, "- [ ] 1\n- [ ]")
-        self.assertEqual(11, result)
+class FindPreviousTaskTest(unittest.TestCase):
+    def test_when_offset_is_in_a_task_ignores_the_task(self):
+        result = task_search.find_previous_task(13, "- [ ] first\n- [ ] second")
+        self.assertEqual(3, result)
+
+    def test_when_no_previous_task_returns_the_original_offset(self):
+        result = task_search.find_previous_task(3, "- [ ] first\n- [ ] second")
+        self.assertEqual(3, result)
 
 
 if __name__ == "__main__":
